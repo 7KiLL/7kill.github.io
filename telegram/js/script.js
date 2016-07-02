45b9b4138c2c02eaf4b361844ae23b14584e9d6e
7KiLL <7killua@gmail.com>
2016-07-03 00:17:08 +0300

    Update to ver 0.2

--- telegram/js/script.js
+++ telegram/js/script.js
@@ -1,76 +1,118 @@
 /**
  * Created by 7KiLL on 27/06/16.
+ * Updated by 7KiLL on 03/07/16. Buttons rework, parse mechanics rework, flexbox,
+ * sticky footer, meta info, contacts.
  */
 
 $( document ).ready(function() {
+    //Global Objects
+    var pattern = {
+        log: /(.+), \[\d{2}\.\d{2}\.\d{2} \d{1,}:\d{1,}\]/,
+        nick: /(.+)(?=(,|:) \[)/,
+        date: /\[\d{2}\.\d{2}\.\d{2} \d{1,}:\d{1,}\]/,
+        reply: /\[In reply to (.+)\]/i,
+        coma: /(,|:)\B/
+    };
+    var parse = {
+        name: function (str) {
+            str = str.replace(pattern.nick,
+            "<td class=\"nickname\">$&</td>");
+            return str;
+        },
+        date: function (str) {
+            str = str.replace(pattern.date,
+            "<td class=\"date\">$&</td>");
+            return str;
+        },
+        clearData: function (str) {
+            str = str.replace(pattern.date,
+            "");
+            return str;
+        },
+        coma: function (str) {
+            str = str.replace(pattern.coma,
+            ""
+            );
+            return str;
+        },
+        reply: function (str) {
+            str = str
+                .replace(pattern.reply,
+            '<em class="text-muted">[In reply to <span class="nickname">$1</span>]</em>');
+            return str;
+        }
+    };
+    var check = {
+        date: function (e) {
+            return $(e).hasClass('checked');
+        },
+        //Deprecated
+        coma: function (e) {
+            return check.date(e);
+        }
+    };
 
+    //Main function
+    function Update(text) {
+        //Check for empty textarea
+        var stringArray = text.split('\n'); 
+        var htmlTable = '';
+        var reply = '';
 
+        $.each(stringArray, function (line, string) {
+            if(pattern.reply.test(string)) {
+                reply = string;
+                reply = parse.reply(reply);
+                string = '';
+            }
+            if(string=='') {return true}
+            else if(pattern.nick.test(string)&&pattern.date.test(string)){
+                string = parse.name(string);
+                string = parse.coma(string);
+                check.date('.dateBox')?
+                    string = parse.date(string) : string = parse.clearData(string);
+                htmlTable += '<tr>' + string;  //Open Table Row
+            }
+            else if(pattern.nick.test(string)) {
+                string = parse.name(string);
+                string = parse.coma(string);
+                htmlTable += '<tr>' + string;  //Open Table Row
+            }
+            else {
+                if(reply) {
+                    string = reply + ' ' + string;
+                    reply = '';
+                }
+                string = '<td class="text-col">' + string + '</td>';
+                htmlTable += string + '</tr>'; //Close Table Row
+            }
+        });
+        htmlTable = '<table class="table table-sm"><tbody>' + htmlTable + '</tbody></table>';
+        return htmlTable;
+    }
+
+    //Updating buttons color
     updateBgColor('#nickColor', 'name');
     updateBgColor('#dateColor', 'date');
     updateBgColor('#textColor', 'text');
 
-
+    //Local storage shit
     if(localStorage.getItem('textareaText'))
         $('#processedText').val(localStorage.getItem('textareaText'));
-
+    //Updating textarea "cache"
     $('#processedText').on('change', function () {
        localStorage.setItem('textareaText', $(this).val());
     });
-
+    //Start button event handler
     $('#startProcess').on('click', function(){
         var text = $('#processedText').val();
-        //Check for empty textarea
         if(!text) {
-            showAlert("Empty field", 'danger', '.row', 3000);
+            showAlert("Empty field", 'danger', '#wrap', 1200);
             return false;
         }
-        var textStrings = text.split('\n');
-        var re =  /(.+), \[\d{2}\.\d{2}\.\d{2} \d{1,}:\d{1,}\]/;
-        var name = /(.+),/;
-        var date = /\[\d{2}\.\d{2}\.\d{2} \d{1,}:\d{1,}\]/;
-        var coma = /,<\/td> <td/;
-        var htmlDate = /<td class=\"date\">\[\d{2}\.\d{2}\.\d{2} \d{1,}:\d{1,}\]<\/td>/;
-        var htmlText = new String();
-
-        textStrings.forEach(function(string, i, textString){
-            if(string=='') {}
-            else if(re.test(string)) {
-
-                string = string.replace(name, "<td class=\"nickname\" id='name'>$&</td>");
-                string = string.replace(date, "<td class=\"date\">$&</td>");
-
-                if(!$('.comaBox').is(":checked")) {
-                    console.log("Btn Checked");
-                    string = string.replace(coma, "</td> <td");
-                }
-
-                if(!$('.dateBox').is(":checked")) {
-                    string = string.replace(htmlDate, "");
-                    $('#dateColor').fadeOut(300);
-                }
-                else {
-                    $('#dateColor').fadeIn(300);
-                }
-
-                //string = "<td class='nick-date-col'>" + string;
-                //string = string + "</td>";
-
-                string = "<tr>" + string;
-        }
-            else {
-                string = "<td class='text-col'>" + string;
-                string = string + "</td>";
-                string = string + "</tr>"
-            }
-            htmlText = htmlText + string;
-
-        });
-        htmlText = "<table class=\"table table-sm\"><tbody>" + htmlText + " </tbody></table>";
-
         $('.form-group').hide();
-        var parsedText = document.createElement('p');
-        parsedText.innerHTML = htmlText;
-        $('.card-block').append(htmlText);
+        $('.table').show();
+        $('.card-block').append(Update(text));
 
         updateColor('.nickname', 'name');
         updateColor('.date', 'date');
@@ -79,18 +121,17 @@ $( document ).ready(function() {
         $('.inputs').show(300);
         $('#editProcess').show(300);
         $('#newProcess').show(300);
-        $('.comaBox').show(300);
     });
-
+    //Edit
     $('#editProcess').on('click', function () {
         $('table').remove();
         $('.form-group').show(300);
         $('#editProcess').hide(300);
         $('#newProcess').hide(300);
         $('#startProcess').show(300);
-        $('.comaBox').hide(300);
-    });
 
+    });
+    //Clear
     $('#newProcess').on('click', function () {
         $('#processedText').val('');
         $('table').remove();
@@ -98,18 +139,28 @@ $( document ).ready(function() {
         $(this).hide(300);
         $('.form-group').show(300);
         $('#startProcess').show(300);
-    });
 
-    $('.comaBox').change(function () {
-        console.log('Checkbox has been clicked');
-        console.log($('.comaBox').is(":checked"));
-        textUpdate();
     });
-    $('.dateBox').change(function () {
+    //Date
+    $('.dateBox').on('click', function () {
+        if(check.date(this)) {
+            $(this)
+                .removeClass('checked btn-success')
+                .addClass('btn-danger')
+                .html('Date <i class="fa fa-times" aria-hidden="true"></i>');
+                $('#dateColor').fadeOut(300);
+        }
+        else {
+            $(this)
+                .removeClass('btn-danger')
+                .addClass('checked btn-success')
+                .html('Date <i class="fa fa-check" aria-hidden="true"></i>');
+            $('#dateColor').fadeIn(300);
+        }
        textUpdate();
     });
 
-    //Functions
+    //Other functions
     function updateColor(e, l) {
         if(localStorage.getItem(l)!==null) {
             console.log('localStorage: ' + l + ' has been found');
@@ -125,76 +176,25 @@ $( document ).ready(function() {
     }
 
     function showAlert(msg, type, where, time) {
-        var alert = '<div class="alert alert-'+ type+'"role="alert">' +
+        var alert = '<div class="alert alert-'+ type+'" role="alert">' +
             '<strong>'+ msg+'</strong>' +
         '</div>';
-        console.log(alert);
-        $(where).append(alert);
-        console.log(where);
-        setTimeout(function() { $('.alert').fadeOut(300 , function(){$(this).remove()}) }, 1000);
-        console.log("Time");
+        $(where).prepend(alert);
+        setTimeout(function() { $('.alert').fadeOut(300 , function(){$(this).remove()}) }, time);
         }
 
     function textUpdate() {
         var text = $('#processedText').val();
-        //Check for empty textarea
         if(!text) {
-            showAlert("Empty field", 'danger', '.row', 3000);
+            showAlert("Empty field", 'danger', '#wrap', 1200);
             return false;
         }
-        var textStrings = text.split('\n');
-        var re =  /(.+), \[\d{2}\.\d{2}\.\d{2} \d{1,}:\d{1,}\]/;
-        var name = /(\w)+( *)(\w)*,/;
-        var date = /\[\d{2}\.\d{2}\.\d{2} \d{1,}:\d{1,}\]/;
-        var htmlDate = /<td class=\"date\">\[\d{2}\.\d{2}\.\d{2} \d{1,}:\d{1,}\]<\/td>/;
-        var coma = /,<\/td> <td/;
-
-        var htmlText = new String();
-
-        textStrings.forEach(function(string, i, textString){
-            if(string=='') {}
-            else if(re.test(string)) {
-
-                string = string.replace(name, "<td class=\"nickname\" id='name'>$&</td>");
-                string = string.replace(date, "<td class=\"date\">$&</td>");
-
-                if(!$('.comaBox').is(':checked')) {
-                    console.log("Checkbox has been clicked");
-                    string = string.replace(coma, "</td> <td");
-                }
-
-                if(!$('.dateBox').is(":checked")) {
-                    string = string.replace(htmlDate, "");
-                    $('#dateColor').fadeOut(300);
-                }
-                else {
-                    $('#dateColor').fadeIn(300);
-                }
-
-                //string = "<td class='nick-date-col'>" + string;
-                //string = string + "</td>";
-
-                string = "<tr>" + string;
-            }
-            else {
-                string = "<td class='text-col'>" + string;
-                string = string + "</td>";
-                string = string + "</tr>"
-            }
-            htmlText = htmlText + string;
-
-        });
-        htmlText = "<table class=\"table table-sm\"><tbody>" + htmlText + " </tbody></table>";
-
         $('.form-group').hide();
-        var parsedText = document.createElement('p');
-        parsedText.innerHTML = htmlText;
-        $('table').remove();
-        $('.card-block').append(htmlText);
+        $('table>tbody').html(Update(text));
         updateColor('.nickname', 'name');
         updateColor('.date', 'date');
         updateColor('.text-col', 'text');
+        $('.footer').addClass('sticky');
     }
-
 
 });
